65c41efdae153eee6798ca2df7e891f1f4e0aaf4
vuhongson2615 <vuhongson2615@gmail.com>
2020-07-09 13:53:33 +0700

    cac bai thuc hanh va bai tap javascript

---
+++ JAVASCRIPT/cacbaithuchanh/js-toantu.js
@@ -0,0 +1,5 @@
+
+let width = prompt("enter the width")
+let height = prompt("enter the height")
+let area = parseInt(width)  * parseInt(height) ;
+document.write("the area is :" + area);
---
+++ JAVASCRIPT/cacbaithuchanh/js.js
@@ -0,0 +1,12 @@
+function checkaler() {
+    alert('xinchao');
+}
+function checkdocument() {
+    document.getElementById("hello").innerHTML = 'xin chao'
+}
+function checkdocumentwrite() {
+    document.write('xin chao')
+}
+function checkconsolelog() {
+    console.log('xin chao')
+}
